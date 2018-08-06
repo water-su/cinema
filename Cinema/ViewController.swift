f7233faa22a97ececaeb317ec98e342c560e1f65
water su <water@deepblu.com>
2018-08-06 16:00:52 +0800

    Initial Commit

---
+++ Cinema/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cinema
+//
+//  Created by Water Su on 2018/8/6.
+//  Copyright © 2018年 iiwa-design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
