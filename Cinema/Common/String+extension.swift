e0b1cc9e7dc947328e85f4a36bc0e10557bbfaaa
water su <water@deepblu.com>
2018-08-09 19:01:05 +0800

    [added]: multi-language support

---
+++ Cinema/Common/String+extension.swift
@@ -0,0 +1,17 @@
+//
+//  String+extension.swift
+//  Cinema
+//
+//  Created by Water Su on 2018/8/9.
+//  Copyright © 2018年 iiwa-design. All rights reserved.
+//
+
+import Foundation
+
+extension String{
+    
+    var localized: String{
+        return NSLocalizedString(self, comment: "")
+    }
+    
+}
